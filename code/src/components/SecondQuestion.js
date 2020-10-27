a4b9e768f6e97eab072cd363422ecbe6072d9af2
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-27 18:37:28 +0100

    Second question styling done

--- code/src/components/SecondQuestion.js
+++ code/src/components/SecondQuestion.js
@@ -10,21 +10,20 @@ export const SecondQuestion = ({number, setNumber, setQuestion}) => {
   };
 
   return (
-    <>
-      <div className="number-picker">
-        <label key={number}>
-          Pick your favorite number (between 1 and 5):
-          <input
-            type="number"
-            name="quantity"
-            min="1"
-            max="5"
-            value={number}
-            onChange={(event) => setNumber(event.target.value)}
-            required />
-        </label>
-        <button onClick={handleClick}>Next</button>
-      </div>
-    </>
+    <div className="welcome-box question-two-container">
+      <label key={number}>
+        Pick your favorite number
+        <p>Between 5 and 10</p>
+        <input
+          type="number"
+          name="quantity"
+          min="5"
+          max="10"
+          value={number}
+          onChange={(event) => setNumber(event.target.value)}
+          required />
+      </label>
+      <button className="start-button second-button" onClick={handleClick}>Next</button>
+    </div>
   );
 };
