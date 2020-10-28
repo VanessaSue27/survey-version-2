b6324663965b3d38375781e35e11dced13c0827d
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-28 09:21:04 +0100

    Final text styling done

--- code/src/components/FinalText.js
+++ code/src/components/FinalText.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export const FinalText = ({destination, number, day, snacks}) => {
   return (
-    <>
-    <h1>Chosen: {destination}, {number}, {day}, {snacks.join(', ')}</h1>
-    <button onClick={()=>window.location.reload()}>Restart</button>
-    </>
+    <section className="welcome-box final-text">
+    <p>During this digital dream getaway, you'll be going to an exotic {destination} destination with {number} of your best friends. There'll be a party on the {day}, where you'll share delicious {snacks.join(', ')} with everyone!</p>
+    <button className="start-button final-button" onClick={()=>window.location.reload()}>Restart</button>
+    </section>
   )
 };
